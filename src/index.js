fad4f8ab4524cc0313211f7185c4e50f30ab3bc0
talgat55 <lightxdesign55@gmail.com>
2019-09-22 15:38:26 +0600

    add redux

--- src/index.js
+++ src/index.js
@@ -3,20 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch, withRouter} from "react-router-dom";
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import 'semantic-ui-css/semantic.min.css'
+import firebase from './firebase';
+import {createStore} from 'redux';
+import {Provider, connect} from 'react-redux';
+import {composeWithDevTools} from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import {setUser} from './actions';
+const store = createStore( rootReducer, composeWithDevTools);
 
+class Root extends React.Component {
+    componentDidMount() {
+        firebase
+            .auth()
+            .onAuthStateChanged(user => {
+                if (user) {
+                    this.props.setUser(user);
+                    this.props.history.push("/")
+                }
+            })
+    }
+.
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={App}/>
+                <Route path="/login" component={Login}/>
+                <Route path="/register" component={Register}/>
+            </Switch>
+        );
+    }
+
+}
+
+const RootWithAuth = withRouter(connect(null,{setUser})(Root));
 ReactDOM.render(
-    <Router>
-        <Switch>
-            <Route  exact path="/" component={App}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/register" component={Register}/>
-        </Switch>
-    </Router>
-
+    <Provider store={store}>
+        <Router>
+            <RootWithAuth/>
+        </Router>
+    </Provider>
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
